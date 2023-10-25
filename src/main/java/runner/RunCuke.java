298ba9519cc3f06e3a763213fdf0793eebe3a143
Nitin Singh <qanitin1@gmail.com>
2023-10-25 23:17:53 +0530

    Removed Allure

--- src/main/java/runner/RunCuke.java
+++ src/main/java/runner/RunCuke.java
@@ -3,7 +3,7 @@ package runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src/main/resources/Features/", glue = "steps", plugin = {
+@CucumberOptions(features = "src/main/resources/Features/PracticeForm.feature", glue = "steps", plugin = {
 		"json:target/cucumber.json",
 		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" }, tags = "@Urgent")
 public class RunCuke extends AbstractTestNGCucumberTests {
