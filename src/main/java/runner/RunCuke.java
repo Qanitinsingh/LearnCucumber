f1b7ac72a541699e35aa82eed46366dfbcfefe9e
Nitin Singh <qanitin1@gmail.com>
2023-10-25 15:53:09 +0530

    Updated the cucumber file path

--- src/main/java/runner/RunCuke.java
+++ src/main/java/runner/RunCuke.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src/main/resources/Features/", glue = "steps", plugin = {
-		"json:target/cucumber-reports/Cucumber.json",
+		"json:target/cucumber.json",
 		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" }, tags = "@Urgent")
 public class RunCuke extends AbstractTestNGCucumberTests {
 
